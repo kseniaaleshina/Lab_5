d28127ca8bb32033345d5000f7c5a409e26ab875
kseniaaleshina <alesha1753@gmail.com>
2024-11-29 20:47:39 +0100

    TASK-5: add min function.

--- 5.1.cpp
+++ 5.1.cpp
@@ -29,3 +29,17 @@ int main() {
     product = min * max;
     cout << product << endl;
 }
+
+double min(double* numbers, int size)
+{
+    int i;
+    double min;
+
+    min = numbers[0];
+    
+    for(i = 0; i < size; i++)
+        if(min > numbers[i])
+            min = numbers[i];
+    
+    return min;
+}
