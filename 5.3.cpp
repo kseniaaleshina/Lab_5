f686864cd6a4dc646a6c427d0a9ce98eb0914dd1
kseniaaleshina <alesha1753@gmail.com>
2024-12-01 18:25:48 +0100

    Added loop that changed positive numbers using their logarithms. Closes #22

--- 5.3.cpp
+++ 5.3.cpp
@@ -22,4 +22,9 @@ int main() {
         for (j = 0; j < columns; j++)
             cout << matrix[i][j] << ' ';
     }
+    
+    for(i = 0; i < rows; i++)
+        for(j = 0; j < columns; j++)
+            if(matrix[i][j] > 0)
+                matrix[i][j] = log(matrix[i][j]);
 }
